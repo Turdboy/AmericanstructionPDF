d2e1d2bd4546afd8e84eb5624ed111e3b14b467a
Turdboy <jamestyler8590@gmail.com>
2025-04-25 16:55:18 -0500

    Downgrade Vite, clean install, fix SPA routing

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
-
